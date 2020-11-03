413cb7ba855aa99c0b57fcbf5676b093c9d81dcd
vuckale <56733276+vuckale@users.noreply.github.com>
2020-11-03 23:50:14 +0100

    getopt for getting current weather

--- main.py
+++ main.py
@@ -44,15 +44,18 @@ def getIcon(weather_condition):
 
 def main():
 	try:
-		opts, args = getopt(sys.argv[1:], "ho:v", ["help", "output="])
+		opts, args = getopt(sys.argv[1:], "cho:v", ["current", "help", "output="])
 	except getopt.GetoptError as err:
 		print(err)
 		sys.exit(2)
 
 	output = None
 	verbose = False
+	current_flag = False
 	for o, a in opts:
-		if o == "-v":
+		if o in ("-c", "--current"):
+			current_flag = True
+		elif o == "-v":
 			print("option -v")
 			verbose = True
 		elif o in ("-h", "--help"):
@@ -65,29 +68,28 @@ def main():
 			assert False, "unhandled option"
 
 	global 	current_dateTime, sunset_dateTime, sunrise_dateTime
-	if not url:
-		print('url not specified: visit https://weather.com/en-GB/, enter your destination and pate url in \'url\' variable')
-	else:
-			try:
-				html_doc = requests.get(url).text
-				soup = BeautifulSoup(html_doc, 'html.parser')
-				sunrise_sunset = soup.find("div", {"class" : "SunriseSunset--datesContainer--3YG_Q"})
-				sunrise = sunrise_sunset.find("div" , {"data-testid" : "SunriseValue"}).p.string
-				sunset = sunrise_sunset.find("div" , {"data-testid" : "SunsetValue"}).p.string
-				sunrise_split = sunrise.split(':')
-				sunrise_dateTime = (datetime.datetime.now()).replace(hour=int(sunrise_split[0]), minute=int(sunrise_split[1]))
-				sunset_split = sunset.split(':')
-				sunset_dateTime = (datetime.datetime.now()).replace(hour=int(sunset_split[0]), minute=(int(sunset_split[1])))
-				current_dateTime = datetime.datetime.now()
-				currentWeather = soup.find("div", { "class" : "CurrentConditions--primary--3xWnK" })
-				temperature = currentWeather.span.string
-				weather_condition = currentWeather.div.string
- 
-				print( getIcon(weather_condition) + ' ' + temperature + 'C')
-			except OSError as e:
-				pass
+	if (current_flag):
+		if not url:
+			print('url not specified: visit https://weather.com/en-GB/, enter your destination and pate url in \'url\' variable')
+		else:
+				try:
+					html_doc = requests.get(url).text
+					soup = BeautifulSoup(html_doc, 'html.parser')
+					sunrise_sunset = soup.find("div", {"class" : "SunriseSunset--datesContainer--3YG_Q"})
+					sunrise = sunrise_sunset.find("div" , {"data-testid" : "SunriseValue"}).p.string
+					sunset = sunrise_sunset.find("div" , {"data-testid" : "SunsetValue"}).p.string
+					sunrise_split = sunrise.split(':')
+					sunrise_dateTime = (datetime.datetime.now()).replace(hour=int(sunrise_split[0]), minute=int(sunrise_split[1]))
+					sunset_split = sunset.split(':')
+					sunset_dateTime = (datetime.datetime.now()).replace(hour=int(sunset_split[0]), minute=(int(sunset_split[1])))
+					current_dateTime = datetime.datetime.now()
+					currentWeather = soup.find("div", { "class" : "CurrentConditions--primary--3xWnK" })
+					temperature = currentWeather.span.string
+					weather_condition = currentWeather.div.string
+					print( getIcon(weather_condition) + ' ' + temperature + 'C')
+				except OSError as e:
+					pass
 
 
 if __name__ == "__main__":
     main()
-
