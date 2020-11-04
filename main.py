8e1a85a56c0dc8f801fd68b8850482a84a2f99ad
vuckale <56733276+vuckale@users.noreply.github.com>
2020-11-04 18:03:51 +0100

    replaced getopt with optparse

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import requests
 import datetime
 from getopt import getopt
 import sys
+from optparse import OptionParser
+import optparse
 
 # global variables
 url = ""
@@ -43,55 +45,46 @@ def getIcon(weather_condition):
 
 
 def main():
-	try:
-		opts, args = getopt(sys.argv[1:], "cho:v", ["current", "help", "output="])
-	except getopt.GetoptError as err:
-		print(err)
-		sys.exit(2)
+	usage = "usage: %prog [options] arg1 arg2"
+	parser = OptionParser(usage=usage)
+	parser.add_option("-c", "--current",
+                  action="store_true", dest="current",
+                  help="print current weather temperature in set location")
+	(options, args) = parser.parse_args()
 
-	if not opts:
-		print("ERROR: no option specified")
+	count_None = 0
+	for key, value in options.__dict__.items():
+		if value != None:
+			continue
+		else:
+			count_None += 1
+
+	if count_None == len(options.__dict__.items()):
+		print('ERROR: at least one option should be provided')
 		sys.exit()
 
-	output = None
-	verbose = False
-	current_flag = False
-	for o, a in opts:
-		if o in ("-c", "--current"):
-			current_flag = True
-		elif o == "-v":
-			print("option -v")
-			verbose = True
-		elif o in ("-h", "--help"):
-			print("option -h, --help")
-			sys.exit()
-		elif o in ("-o", "--output"):
-			print("option -o, --output")
-			output = a
-		else:
-			assert False, "unhandled option"
 	global 	current_dateTime, sunset_dateTime, sunrise_dateTime
-	if (current_flag):
-		if not url:
+	if not url:
 			print('url not specified: visit https://weather.com/en-GB/, enter your destination and pate url in \'url\' variable')
-		else:
-				try:
-					html_doc = requests.get(url).text
-					soup = BeautifulSoup(html_doc, 'html.parser')
-					sunrise_sunset = soup.find("div", {"class" : "SunriseSunset--datesContainer--3YG_Q"})
-					sunrise = sunrise_sunset.find("div" , {"data-testid" : "SunriseValue"}).p.string
-					sunset = sunrise_sunset.find("div" , {"data-testid" : "SunsetValue"}).p.string
-					sunrise_split = sunrise.split(':')
-					sunrise_dateTime = (datetime.datetime.now()).replace(hour=int(sunrise_split[0]), minute=int(sunrise_split[1]))
-					sunset_split = sunset.split(':')
-					sunset_dateTime = (datetime.datetime.now()).replace(hour=int(sunset_split[0]), minute=(int(sunset_split[1])))
-					current_dateTime = datetime.datetime.now()
-					currentWeather = soup.find("div", { "class" : "CurrentConditions--primary--3xWnK" })
-					temperature = currentWeather.span.string
-					weather_condition = currentWeather.div.string
-					print( getIcon(weather_condition) + ' ' + temperature + 'C')
-				except OSError as e:
-					pass
+	else:
+		try:
+			html_doc = requests.get(url).text
+			soup = BeautifulSoup(html_doc, 'html.parser')
+			sunrise_sunset = soup.find("div", {"class" : "SunriseSunset--datesContainer--3YG_Q"})
+			sunrise = sunrise_sunset.find("div" , {"data-testid" : "SunriseValue"}).p.string
+			sunset = sunrise_sunset.find("div" , {"data-testid" : "SunsetValue"}).p.string
+			sunrise_split = sunrise.split(':')
+			sunrise_dateTime = (datetime.datetime.now()).replace(hour=int(sunrise_split[0]), minute=int(sunrise_split[1]))
+			sunset_split = sunset.split(':')
+			sunset_dateTime = (datetime.datetime.now()).replace(hour=int(sunset_split[0]), minute=(int(sunset_split[1])))
+			current_dateTime = datetime.datetime.now()
+			currentWeather = soup.find("div", { "class" : "CurrentConditions--primary--3xWnK" })
+			temperature = currentWeather.span.string
+			weather_condition = currentWeather.div.string
+		except OSError as e:
+			pass
+		if options.current:
+			print(getIcon(weather_condition) + ' ' + temperature + 'C')
 
 
 if __name__ == "__main__":
